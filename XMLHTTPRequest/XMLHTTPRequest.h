240c32b824a1701d79262d83f98fa0cfcb517c06
max lin <lindahai0512@gmail.com>
2018-01-18 20:37:33 +0800

    XMLHTTPRequest For iOS and macOS

---
+++ XMLHTTPRequest/XMLHTTPRequest.h
@@ -0,0 +1,36 @@
+#import <Foundation/Foundation.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+
+
+typedef NS_ENUM(NSUInteger , ReadyState) {
+    XMLHttpRequestUNSENT =0,	// open()has not been called yet.
+    XMLHttpRequestOPENED,	    // send()has not been called yet.
+    XMLHttpRequestHEADERS,      // RECEIVED	send() has been called, and headers and status are available.
+    XMLHttpRequestLOADING,      // Downloading; responseText holds partial data.
+    XMLHttpRequestDONE          // The operation is complete.
+};
+
+@protocol XMLHttpRequest <JSExport>
+@property (nonatomic) NSString *responseText;
+@property (nonatomic) JSValue *onreadystatechange;
+@property (nonatomic) NSNumber *readyState;
+@property (nonatomic) JSValue *onload;
+@property (nonatomic) JSValue *onerror;
+@property (nonatomic) NSNumber *status;
+
+
+-(void)open:(NSString *)httpMethod :(NSString *)url :(bool)async;
+-(void)send:(id)data;
+-(void)setRequestHeader: (NSString *)name :(NSString *)value;
+-(NSString *)getAllResponseHeaders;
+-(NSString *)getReponseHeader:(NSString *)name;
+@end
+
+
+
+@interface XMLHttpRequest : NSObject <XMLHttpRequest>
+
+- (instancetype)initWithURLSession: (NSURLSession *)urlSession;
+
+- (void)extend:(id)jsContext;
+@end
